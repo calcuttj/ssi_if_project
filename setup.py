e7a7867f883149a0b556a47c3edbb4f28b2c708c
Jake Calcutt <calcuttj@msu.edu>
2023-08-10 18:12:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DataPrep',
-    version='0.1',
+    version='0.2',
     author='calcuttj',
     description='if project',
     packages=find_packages(),
