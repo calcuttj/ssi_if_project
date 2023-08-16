fd464461f2bc76cb50af7433d68c3574d33b431c
Jake <calcuttj@msu.edu>
2023-08-16 18:09:50 -0400

    Plotting updates

--- Model/plot.py
+++ Model/plot.py
@@ -1,13 +1,33 @@
 import h5py as h5
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
+from argparse import ArgumentParser as ap
 
+
+def plot_flat(fin):
+  plt.plot(np.array(fin['losses']).flatten(), label='Total loss')
+  plt.plot(np.array(fin['node_losses']).flatten(), label='Node loss')
+  plt.plot(np.array(fin['edge_losses']).flatten(), label='Edge loss')
+
+def plot_ave(fin):
+  losses = np.array(fin['losses'])
+  xs = [len(losses[0])]
+  for i in range(1, len(losses)):
+    xs.append(sum(xs) + len(losses[i]))
+  xs = np.array(xs)
+  plt.scatter(xs, np.mean(fin['losses'], axis=1), label='Total loss', marker='o', color='pink')
+  plt.scatter(xs, np.mean(fin['node_losses'], axis=1), label='Node loss', marker='o', color='pink')
+  plt.scatter(xs, np.mean(fin['edge_losses'], axis=1), label='Edge loss', marker='o', color='pink')
 
 if __name__ == '__main__':
-  with h5.File(sys.argv[1], 'r') as fin:
-    plt.plot(np.array(fin['losses']).flatten(), label='Total loss')
-    plt.plot(np.array(fin['node_losses']).flatten(), label='Node loss')
-    plt.plot(np.array(fin['edge_losses']).flatten(), label='Edge loss')
+  parser = ap()
+  parser.add_argument('-i', required=True)
+  parser.add_argument('--ave', action='store_true')
+  args = parser.parse_args()
+
+  with h5.File(args.i, 'r') as fin:
+    plot_flat(fin)
+    if args.ave:
+      plot_ave(fin)
     plt.legend()
     plt.show()
